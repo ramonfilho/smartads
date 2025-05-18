3e657dbc87af8cb4908360066ab364cad41e50c5
Ramon Moreira Filho <89781420+ramonfilho@users.noreply.github.com>
2025-05-18 15:18:32 -0300

    same

--- scripts/02_preprocessing.py
+++ scripts/02_preprocessing.py
@@ -260,9 +260,9 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Aplicar pipeline de pré-processamento nos conjuntos de dados.")
     parser.add_argument("--input-dir", type=str, default=os.path.join(os.path.expanduser("~"), "desktop/smart_ads/data/01_split"), 
                         help="Diretório contendo os arquivos de entrada (train.csv, validation.csv, test.csv)")
-    parser.add_argument("--output-dir", type=str, default=os.path.join(os.path.expanduser("~"), "desktop/smart_ads/data/02_1_processed"), 
+    parser.add_argument("--output-dir", type=str, default=os.path.join(os.path.expanduser("~"), "desktop/smart_ads/data/02_processed"), 
                         help="Diretório para salvar os arquivos processados")
-    parser.add_argument("--params-dir", type=str, default=os.path.join(os.path.expanduser("~"), "desktop/smart_ads/src/preprocessing/preprocessing_params"), 
+    parser.add_argument("--params-dir", type=str, default=os.path.join(os.path.expanduser("~"), "desktop/smart_ads/src/preprocessing/02_params"), 
                         help="Diretório para salvar os parâmetros aprendidos")
     parser.add_argument("--preserve-text", action="store_true", default=True,
                         help="Preservar as colunas de texto originais (default: True)")
