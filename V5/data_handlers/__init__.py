f487912b5cd8392bc52ea1b2c70578128ccd2baa
Ramon Moreira Filho <89781420+ramonfilho@users.noreply.github.com>
2025-09-16 08:38:34 -0300

    reorganização

--- V5/data_handlers/__init__.py
+++
@@ -1,9 +0,0 @@
-# smart_ads_pipeline/data_handlers/__init__.py
-
-from .data_loader import DataLoader
-from .data_matcher import DataMatcher
-
-__all__ = [
-    'DataLoader',
-    'DataMatcher'
-]
--- V5/pipelines/__init__.py
+++
@@ -1,7 +0,0 @@
-# smart_ads_pipeline/pipelines/__init__.py
-
-from .training_pipeline import TrainingPipeline
-
-__all__ = [
-    'TrainingPipeline'
-]
